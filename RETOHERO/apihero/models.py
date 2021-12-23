324b24e027889c20a9ee814a8fbada8d7b04456a
Valentin <valenp97@gmail.com>
2021-12-23 20:12:34 +0100

    creado modelo Hero

--- RETOHERO/apihero/models.py
+++ RETOHERO/apihero/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Hero(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    alias = models.CharField(max_length=50)
